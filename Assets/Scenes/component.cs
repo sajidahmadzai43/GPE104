fee69b0b056e2e7b52e74c96bc1630bceb0eae26
Sajid Ahmadzai <144490066+sajidahmadzai43@users.noreply.github.com>
2025-07-01 15:07:29 -0700

    Add files via upload

---
+++ Assets/Scenes/component.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello, Unity! This is my first script.");
+    }
+}
